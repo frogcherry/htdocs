88108b8a9c45506762b0929a6dd7ce2a93d86975
Xiaolong <frogcherry@gmail.com>
2013-09-24 10:06:19 +0800

    从excel生成json数据

--- mkjsoninfo/js/index.js
+++ mkjsoninfo/js/index.js
@@ -1,12 +1,30 @@
 function trans(){
 	var text = $("#indata").val();
 	var jdata = mkJson(text);
+	$("#outdata").val(JSON.stringify(jdata));
 }
 
 function mkJson(text){
+	var data = {
+	    locId : "",
+	    svg : "",
+	    linked : [],
+	    info : []
+	};
+	
 	var rows = text.split("\n");
-	for ( var row in rows) {
-		var items = row.split("\t");
+	for (var i = 1; i<rows.length; i++) {
+		var items = rows[i].split("\t");
+		if (items.length < 3) {
+			continue;
+		}
+		var info = {
+			name : 	items[0],
+			summary : items[1],
+			detail : items[2]
+		};
+		data.info.push(info);
 	}
-	alert(rows);
+	
+	return data;
 }
