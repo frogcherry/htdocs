387c1106eb7680d309a3f241a0c9c8846ba5cede
Xiaolong <frogcherry@gmail.com>
2013-09-26 16:22:50 +0800

    nav effect

--- yxcfidc/js/ct.js
+++ yxcfidc/js/ct.js
@@ -1,5 +1,12 @@
+$(document).ready(function(){
+	$("#menu .li1").addClass("current_page_item");
+});
+
 function navTo(id){
+	// effect
 	$("#menu li").removeClass("current_page_item");
-	
 	$("#menu .li"+id).addClass("current_page_item");
+	
+	// nav
+	
 }
