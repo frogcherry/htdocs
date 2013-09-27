a71a0cbbc1e2df9db2f3cda199b221ecb555dec4
Xiaolong Zhou <frogcherry@gmail.com>
2013-09-27 22:26:23 +0800

    去掉多余的注释

--- yxcfidc/js/ct.js
+++ yxcfidc/js/ct.js
@@ -143,21 +143,14 @@ function showCTMap(id){
 			gallery: {
 				enabled: true,
 				navigateByImgClick: true,
-				preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+				preload: [0,1] 
 			},
 			zoom: {
-				    enabled: true, // By default it's false, so don't forget to enable it
-
-				    duration: 300, // duration of the effect, in milliseconds
-				    easing: 'ease-in-out', // CSS transition easing function 
-
-				    // The "opener" function should return the element from which popup will be zoomed in
-				    // and to which popup will be scaled down
-				    // By defailt it looks for an image tag:
+				    enabled: true, 
+				    duration: 300, 
+				    easing: 'ease-in-out', 
 				    opener: function(openerElement) {
-				      // openerElement is the element on which popup was initialized, in this case its <a> tag
-				      // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-				      return openerElement.is('img') ? openerElement : openerElement.find('img');
+				    	return openerElement.is('img') ? openerElement : openerElement.find('img');
 				    }
 			},
 
